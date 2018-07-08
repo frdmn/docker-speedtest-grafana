34f3ab54f68eebb3b3382e97ee4bbe97ca026fd0
Jonas Friedmann <j@frd.mn>
2018-07-08 22:12:29 +0200

    Initial commit

---
+++ docker/speedtest/scripts/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+while :; do
+	echo "[Info][$(date)] Starting speedtest..."
+	OUTPUT = $(/app/speedtest_cli.py)
+	DOWNLOAD=$(echo "${OUTPUT}" | grep "Download:" | awk -F " " '{print $2}')
+	UPLOAD=$(echo "$OUTPUT" | grep "Upload:" | awk -F " " '{print $2}')
+	echo "[Info][$(date)] Speedtest results - Download: ${DOWNLOAD}, Upload: ${UPLOAD}"
+	curl -sL -XPOST 'http://influxdb:8086/write?db=speedtest' --data-binary "download,host=local value=${DOWNLOAD}"
+	curl -sL -XPOST 'http://influxdb:8086/write?db=speedtest' --data-binary "upload,host=local value=${UPLOAD}"
+	echo "[Info][$(date)] Sleeping for ${SPEEDTEST_INTERVAL} seconds..."
+	sleep ${SPEEDTEST_INTERVAL}
+done
