517faac057c97e36e87daeb576b4bdf3ebf338d1
Jonas Friedmann <j@frd.mn>
2018-07-08 23:44:41 +0200

    Use speed-test NPM package

--- docker/speedtest/run.sh
+++ docker/speedtest/run.sh
@@ -2,12 +2,14 @@
 
 while :; do
 	echo "[Info][$(date)] Starting speedtest..."
-	OUTPUT=$(/app/speedtest_cli.py)
-	DOWNLOAD=$(echo "${OUTPUT}" | grep "Download:" | awk -F " " '{print $2}')
-	UPLOAD=$(echo "$OUTPUT" | grep "Upload:" | awk -F " " '{print $2}')
-	echo "[Info][$(date)] Speedtest results - Download: ${DOWNLOAD}, Upload: ${UPLOAD}"
+	JSON=$(speed-test -j)
+	DOWNLOAD=$(echo "${JSON}" | json download)
+	UPLOAD=$(echo "$JSON" | json upload)
+	PING=$(echo "${JSON}" | json ping)
+	echo "[Info][$(date)] Speedtest results - Download: ${DOWNLOAD}, Upload: ${UPLOAD}, Ping: ${PING}"
 	curl -sL -XPOST 'http://influxdb:8086/write?db=speedtest' --data-binary "download,host=local value=${DOWNLOAD}"
 	curl -sL -XPOST 'http://influxdb:8086/write?db=speedtest' --data-binary "upload,host=local value=${UPLOAD}"
+	curl -sL -XPOST 'http://influxdb:8086/write?db=speedtest' --data-binary "ping,host=local value=${PING}"
 	echo "[Info][$(date)] Sleeping for ${SPEEDTEST_INTERVAL} seconds..."
 	sleep ${SPEEDTEST_INTERVAL}
 done
