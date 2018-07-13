9681e944e9565cb22e960d6e66f8762f4025158b
QuadStingray <github@quadstingray.com>
2018-07-13 22:47:32 +0200

    moved some settings to enviroments

--- docker/speedtest/run.sh
+++ docker/speedtest/run.sh
@@ -7,9 +7,9 @@ while :; do
 	UPLOAD=$(echo "$JSON" | json upload)
 	PING=$(echo "${JSON}" | json ping)
 	echo "[Info][$(date)] Speedtest results - Download: ${DOWNLOAD}, Upload: ${UPLOAD}, Ping: ${PING}"
-	curl -sL -XPOST 'http://influxdb:8086/write?db=speedtest' --data-binary "download,host=local value=${DOWNLOAD}"
-	curl -sL -XPOST 'http://influxdb:8086/write?db=speedtest' --data-binary "upload,host=local value=${UPLOAD}"
-	curl -sL -XPOST 'http://influxdb:8086/write?db=speedtest' --data-binary "ping,host=local value=${PING}"
+	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "download,host=${SPEEDTEST_HOST} value=${DOWNLOAD}"
+	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "upload,host=${SPEEDTEST_HOST} value=${UPLOAD}"
+	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "ping,host=${SPEEDTEST_HOST} value=${PING}"
 	echo "[Info][$(date)] Sleeping for ${SPEEDTEST_INTERVAL} seconds..."
 	sleep ${SPEEDTEST_INTERVAL}
 done
